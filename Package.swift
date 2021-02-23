e474b999b57179890b1818b06f759bf0bd5fbc35
Tim <0xtimc@gmail.com>
2021-02-23 17:45:15 +0000

    Package rename

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SotoElasticsearch",
+    name: "SotoElasticsearchClient",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SotoElasticsearch",
-            targets: ["SotoElasticsearch"]),
+            name: "SotoElasticsearchClient",
+            targets: ["SotoElasticsearchClient"]),
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
@@ -19,13 +19,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SotoElasticsearch",
+            name: "SotoElasticsearchClient",
             dependencies: [
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "SotoElasticsearchService", package: "soto"),
             ]),
         .testTarget(
-            name: "SotoElasticsearchTests",
-            dependencies: ["SotoElasticsearch"]),
+            name: "SotoElasticsearchClientTests",
+            dependencies: ["SotoElasticsearchClient"]),
     ]
 )
