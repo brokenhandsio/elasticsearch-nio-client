e207fa007d7af01d5ae84f90c29977202c8d9b1f
Tim <0xtimc@gmail.com>
2021-02-23 17:37:51 +0000

    Add script to start local ES container for testing

---
+++ scripts/startLocalDockerESTest.swift
@@ -0,0 +1,43 @@
+#!/usr/bin/swift
+import Foundation
+
+let port = 9200
+let containerName = "soto-esclient-test"
+
+print("Starting local Elasticsearch instance in container \(containerName)")
+
+@discardableResult
+func shell(_ args: String..., returnStdOut: Bool = false) -> (Int32, Pipe) {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+  let pipe = Pipe()
+  if returnStdOut {
+      task.standardOutput = pipe
+  }
+  task.launch()
+  task.waitUntilExit()
+  return (task.terminationStatus, pipe)
+}
+
+extension Pipe {
+    func string() -> String? {
+        let data = self.fileHandleForReading.readDataToEndOfFile()
+        let result: String?
+        if let string = String(data: data, encoding: String.Encoding.utf8) {
+            result = string
+        } else {
+            result = nil
+        }
+        return result
+    }
+}
+
+let (dockerResult, _) = shell("docker", "run", "--name", containerName, "-p", "\(port):9200", "-e", "discovery.type=single-node", "-d", "docker.elastic.co/elasticsearch/elasticsearch:7.6.2")
+
+guard dockerResult == 0 else {
+    print("❌ ERROR: Failed to create the Elasticsearch instance")
+    exit(1)
+}
+
+print("Elasticsearch created in Docker 🐳")
