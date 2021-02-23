e8a13553ef458ca577248d57ea136a6a8d9100ee
Tim <0xtimc@gmail.com>
2021-02-23 17:17:39 +0000

    Add Soto and AHC as dependencies

--- Package.swift
+++ Package.swift
@@ -12,15 +12,18 @@ let package = Package(
             targets: ["SotoElasticsearch"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
+        .package(url: "https://github.com/soto-project/soto.git", from: "5.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SotoElasticsearch",
-            dependencies: []),
+            dependencies: [
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "SotoElasticsearchService", package: "soto"),
+            ]),
         .testTarget(
             name: "SotoElasticsearchTests",
             dependencies: ["SotoElasticsearch"]),
