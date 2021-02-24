517d358a92c592cb773f7f9f4eaddaedcaecf9ce
Tim <0xtimc@gmail.com>
2021-02-24 10:22:22 +0000

    Improve the count response

--- Sources/SotoElasticsearchClient/Models/ESCountResponse.swift
+++ Sources/SotoElasticsearchClient/Models/ESCountResponse.swift
@@ -2,4 +2,10 @@ import Foundation
 
 public struct ESCountResponse: Codable {
     public let count: Int
+    public let shards: ESShardsResponse
+
+    enum CodingKeys: String, CodingKey {
+        case count
+        case shards = "_shards"
+    }
 }
