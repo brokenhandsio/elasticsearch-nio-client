a5e2877fc698de73563491aa7dc561d2ceddfe81
niklhut <49069026+niklhut@users.noreply.github.com>
2022-12-22 16:28:34 +0100

    Retrieve total count from multiple documents response

--- Sources/ElasticsearchNIOClient/Models/ESMultipleDocumentResponse.swift
+++ Sources/ElasticsearchNIOClient/Models/ESMultipleDocumentResponse.swift
@@ -2,6 +2,12 @@ import Foundation
 
 public struct ESGetMultipleDocumentsResponse<Document: Decodable>: Decodable {
     public struct Hits: Decodable {
+        public struct Total: Decodable {
+            public let value: Int
+            public let relation: String
+        }
+
+        public let total: Total
         public let hits: [ESGetSingleDocumentResponse<Document>]
     }
 
